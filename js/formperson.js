a8d3ed11aea5076771e5ff02007890f40c2e7ce3
LinhVuHoang <56089011+LinhVuHoang@users.noreply.github.com>
2022-03-04 14:23:34 +0700

    update client person

---
+++ js/formperson.js
@@ -0,0 +1,35 @@
+document.addEventListener('DOMContentLoaded',function (){
+    var btnSubmit = document.getElementById("btn-submit");
+    var txtName = document.forms['person-form']['name'];
+    var txtAddress = document.forms['person-form']['address'];
+    var txtMobilephone = document.forms['person-form']['mobilephone'];
+    var txtCmnd = document.forms['person-form']['cmnd'];
+    var txtBudget= document.forms['person-form']['budget'];
+    var btnsubmit = document.getElementById("btn-submit");
+    btnsubmit.onclick = function () {
+        var name = txtName.value;
+        var address = txtAddress.value;
+        var mobilephone = txtMobilephone.value;
+        var cmnd = txtCmnd.value;
+        var budget = txtBudget.value;
+
+        var dataToSend = {
+            "name": name,
+            "address":address,
+            "mobilephone":mobilephone,
+            "cmnd":cmnd,
+            "budget":budget
+        }
+        var xmlHttpRequest = new XMLHttpRequest();
+        xmlHttpRequest.onreadystatechange = function () {
+            //kiểm tra khi trạng thái request đã hoàn thành và thành công(status =200) hoặc thất bại (404,500...)
+            if (xmlHttpRequest.readyState == 4 && xmlHttpRequest.status == 201) {
+                alert('Create new person success!');
+                window.location.href = "/asgnhom/personlist.html";
+            }
+        }
+        xmlHttpRequest.open('post','http://localhost:8080/api/v1/persons',false)
+        xmlHttpRequest.setRequestHeader('Content-Type','application/json');
+        xmlHttpRequest.send(JSON.stringify(dataToSend));
+    }
+})
---
+++ js/personlist.js
@@ -0,0 +1,26 @@
+document.addEventListener('DOMContentLoaded',function (){
+
+
+    var tableBody = document.getElementById('my-table-data');
+    var xmlHttpRequest = new XMLHttpRequest();
+    xmlHttpRequest.onreadystatechange = function (){
+        if(xmlHttpRequest.readyState ==4 && xmlHttpRequest.status ==200){
+            var data = JSON.parse(xmlHttpRequest.responseText);
+            var newcontent ='';
+            for(let i =0;i<data.length;i++){
+                newcontent +=`<tr>
+<td>${data[i].id}</td>
+<td>${data[i].name}</td>
+<td>${data[i].address}</td>
+<td>${data[i].mobilephone}</td>
+<td>${data[i].cmnd}</td>
+<td>${data[i].budget}</td>
+</tr>
+`;
+            }
+            tableBody.innerHTML = newcontent;
+        }
+    }
+    xmlHttpRequest.open('get','http://localhost:8080/api/v1/persons',false)
+    xmlHttpRequest.send();
+})
